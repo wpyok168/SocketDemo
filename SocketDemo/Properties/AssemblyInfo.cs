2bfbc5decc0a3552413967d423a85a04b72e89ed
wpyok500 <wpyok500@gmail.com>
2024-12-02 13:16:17 +0800

    up

--- SocketDemo/Properties/AssemblyInfo.cs
+++ SocketDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SocketDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Get _IdentifyingNumber_Server")]
+[assembly: AssemblyDescription("fujian-xi")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SocketDemo")]
+[assembly: AssemblyCompany("福建-兮")]
+[assembly: AssemblyProduct("Get _IdentifyingNumber")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
